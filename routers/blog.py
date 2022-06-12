46c77cb161de5b7bc133a19f925558001e32e32e
Piyush <piyush.mehra13@gmail.com>
2022-06-12 16:52:46 +0530

    Fixed some bugs.

--- routers/blog.py
+++ routers/blog.py
@@ -8,16 +8,18 @@ import database
 import models
 
 
-router = APIRouter()
+router = APIRouter(
+    tags=['blogs']
+)
 
 
-@router.get('/blog/',tags=["Blogs"])
+@router.get('/blog/')
 def get_blog(db:Session=Depends(database.get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@router.post('/',status_code=status.HTTP_201_CREATED,tags=["Blogs"]) 
+@router.post('/',status_code=status.HTTP_201_CREATED) 
 def create(req:schemas.Blog,db:Session=Depends(database.get_db)):
     new_blog = Blog(title=req.title,body=req.body,user_id=1)
     db.add(new_blog)
@@ -26,7 +28,7 @@ def create(req:schemas.Blog,db:Session=Depends(database.get_db)):
     return new_blog
 
 
-@router.get('/blog/{id}',response_model=schemas.ShowBlog,status_code=200,tags=["Blogs"])
+@router.get('/blog/{id}',response_model=schemas.ShowBlog,status_code=200)
 def show(id, response:Response, db:Session=Depends(database.get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -34,7 +36,7 @@ def show(id, response:Response, db:Session=Depends(database.get_db)):
         return {"msg" : "data not found"}
     return blog
 
-@router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=["Blogs"])
+@router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id ,response:Response, db:Session=Depends(database.get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
     db.commit()
@@ -43,7 +45,7 @@ def destroy(id ,response:Response, db:Session=Depends(database.get_db)):
         return {"msg" : "data not found"}
     return blog
 
-@router.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=["Blogs"])
+@router.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update(id, response:Response, req:schemas.Blog, db:Session=Depends(database.get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).update({"title":req.title,"body":req.body})
     db.commit()
--- routers/user.py
+++ routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import Depends,status,Response
+from fastapi import Depends, status, Response
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from models import User
@@ -7,20 +7,25 @@ import schemas
 import database
 import models
 
-router = APIRouter()
+router = APIRouter(
+    tags=["User"]
+)
 
-@router.post('/user/',tags=["User"])
-def create_user(req:schemas.User, db:Session=Depends(database.get_db)):
-    user = User(name=req.name,email=req.email,password=hashing.bcrypt(req.password))
+
+@router.post('/user/')
+def create_user(req: schemas.User, db: Session = Depends(database.get_db)):
+    user = User(name=req.name, email=req.email,
+                password=hashing.bcrypt(req.password))
     db.add(user)
     db.commit()
     db.refresh(user)
-    return {"msg":"Data updated","data":user}
+    return {"msg": "Data updated", "data": user}
 
-@router.get('/user/{id}',tags=["User"])
-def get_user(id:int, response:Response, db:Session=Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.id==id).first()
+
+@router.get('/user/{id}')
+def get_user(id: int, response: Response, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"msg" : "data not found"}
-    return {"msg":"success","data":user}
+        return {"msg": "data not found"}
+    return {"msg": "success", "data": user}
